11aad2cf772f4c4bfb4e9e96fe9a6244b02bff90
Chenyu Shi <shounshi2017@gmail.com>
2019-02-17 22:52:22 -0800

    saving comments

--- AmphibiaWeb2Tests/AmphibiaWeb2Tests.h
+++ AmphibiaWeb2Tests/AmphibiaWeb2Tests.h
@@ -5,9 +5,12 @@
 //  Created by Mel Roderick on 11/3/12.
 //  Copyright (c) 2012 Mel Roderick. All rights reserved.
 //
+// ######
+// First of all the testing cases are noexistent. Second the testing framework is deprecated and removed
+// 
+// ######
+// #import <SenTestingKit/SenTestingKit.h>
 
-#import <SenTestingKit/SenTestingKit.h>
-
-@interface AmphibiaWeb2Tests : SenTestCase
+@interface AmphibiaWeb2Tests : NSObject
 
 @end
